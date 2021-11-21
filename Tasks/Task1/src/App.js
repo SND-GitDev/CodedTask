5b0598bcc6eeae598d76c09fd3f5e56b3dc2432f
unknown <sws.indiedev@gmail.com>
2021-11-21 16:04:43 +0300

    UPDATED Task1 end result

--- Tasks/Task1/src/App.js
+++ Tasks/Task1/src/App.js
@@ -1,28 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card'
 
 import { FaApple } from "react-icons/fa";
 
-export default class Apple extends React.Component {
+export default function Apple () {
 
-  constructor(props) {
-    super(props);
-    this.state = {color: "grey"};
-  }
+  const [color, setColor] = useState("grey");
 
-  render() {
-    return (
-      <Card className="card border-light mb-3 text-center">
-        <Card.Body className="shadow p-3 mb-5 bg-white rounded align-middle">
-          <div><FaApple size={200} color={this.state.color}/></div>
-          <div>
-            <Button variant="primary" onClick = {() => this.setState({color: "blue"})}>Blue</Button>
-            <Button variant="success" onClick = {() => this.setState({color: "green"})}>Green</Button>
-            <Button variant="danger" onClick = {() => this.setState({color: "red"})}>Red</Button>
-          </div>
-        </Card.Body>
-      </Card>
-    );
-  }
+  return (
+    <Card className="card border-light mb-3 text-center">
+      <Card.Body className="shadow p-3 mb-5 bg-white rounded align-middle">
+        <div><FaApple size={200} color={color}/></div>
+        <div>
+          <Button variant="primary" onClick = {() => setColor("blue")}>Blue</Button>
+          <Button variant="success" onClick = {() => setColor("green")}>Green</Button>
+          <Button variant="danger" onClick = {() => setColor("red")}>Red</Button>
+        </div>
+      </Card.Body>
+    </Card>
+  );
 }
